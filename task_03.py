331630d1f133acf9576cbd7810b95b415b1869bb
williamjcole <billy.cole@gmail.com>
2016-02-04 20:30:51 +0000

    added standard headding to task_03.py

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is a simple Python program."""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
