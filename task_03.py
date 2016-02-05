3ca957fa532f3c4f8ab13dd314941347111468ac
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-02-05 04:33:27 +0000

    Uploading Syn2 files

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A simple Python file."""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
