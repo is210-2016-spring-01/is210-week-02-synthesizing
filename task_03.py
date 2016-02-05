50a0f2388e0cc4a3539873748fa8c8be16a3c654
Nicole Parrilla <nicole.parrilla@spsmail.cuny.edu>
2016-02-05 04:44:30 +0000

    Fixed errors- whitespace, interpreter, encoding, doctring, wrong word

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Running charlie, violet, patricia and linus"""
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
