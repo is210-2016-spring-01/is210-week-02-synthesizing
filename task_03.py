12b4a939499d656d88e4268fab8b3582b4446e46
acodyjr <acody@me.com>
2016-02-06 00:22:36 +0000

    Repaired errors to pass pylint and nosetests

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Sets global variable values"""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
