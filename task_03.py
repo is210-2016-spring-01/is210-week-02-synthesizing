994817fd6cabfc5e1e8515deed6dce346859a0c8
Wei Chen <Wei Chen>
2016-02-08 01:34:43 +0000

    created task 1 in html & rst, and modified task_02.py & task_03.py accordingly

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+""" Docstring """
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
