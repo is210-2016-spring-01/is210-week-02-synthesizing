9046a5de736c871f5ef0f261273147942924f376
Vanessa Zuniga <vanessa.zuniga@spsmail.cuny.edugit commit -m added>
2016-02-11 20:37:54 +0000

    changed file

--- task_03.py
+++ task_03.py
@@ -1,7 +1,11 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Making changes to this file."""
+
+CHARLIE = "Chocolate Factory"
+
 VIOLET = "Gray"
+
 PATRICIA = "Reichardt"
+
 LINUS = "van Pelt"
-
-
-
